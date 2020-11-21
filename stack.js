80ef03ca2aeb045cfc27b71fb3af2471652196ef
Kenny Ngo <kenny.ngo.design@gmail.com>
2020-11-21 00:21:23 -0800

    adding to stack

--- stack.js
+++ stack.js
@@ -8,6 +8,28 @@ var Stack = function () {
     this.count++;
   };
 
+  this.pop = function () {
+    if (this.count === 0) {
+      // if returned from the function, rest of code won't run from this function
+      return undefined;
+    }
+
+    this.count--;
+    // aux variable to hold value as 'delete' keyword removes item from array
+    let result = this.storage[this.count];
+    delete this.storage[this.count];
+    return result;
+  };
+
+  this.size = function () {
+    return this.count;
+  };
+
+  // return the value at the end of the stack
+  this.peek = function () {
+    return this.storage[this.count - 1];
+  };
+
   console.log(this);
 };
 
