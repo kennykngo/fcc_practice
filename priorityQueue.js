4aaa8d5900eb6da5f51b17e467192ccda7cb3f72
Kenny Ngo <kenny.ngo.design@gmail.com>
2020-11-23 21:15:50 -0800

    finished priorityQueue

--- priorityQueue.js
+++ priorityQueue.js
@@ -27,4 +27,25 @@ function PriorityQueue() {
     var value = collection.shift();
     return value[0];
   };
+
+  this.front = function () {
+    return collection[0];
+  };
+
+  this.size = function () {
+    return collection.length;
+  };
+
+  this.isEmpty = function () {
+    return collection.length === 0;
+  };
 }
+
+var pq = new PriorityQueue();
+pq.enqueue(["Beau Carnes", 2]);
+pq.enqueue(["Quincy Larson", 3]);
+pq.enqueue(["briana swift", 1]);
+pq.printCollection();
+console.log(pq.dequeue());
+pq.front();
+pq.printCollection();
