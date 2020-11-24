caeb0921fa4cd7e22f1a624de01aba40981b5e11
Kenny Ngo <kenny.ngo.design@gmail.com>
2020-11-24 13:43:21 -0800

    creating add section of BST

---
+++ binarySearchTree.js
@@ -0,0 +1,27 @@
+class Node {
+  constructor(data, left = null, right = null) {
+    this.data = data;
+    this.left = left;
+    this.right = right;
+  }
+}
+
+class BST {
+  constructor() {
+    this.root = null;
+  }
+
+  add(data) {
+    const node = this.root;
+    if (node === null) {
+      this.root = new Node(data);
+      return;
+    } else {
+      const searchTree = function (node) {
+        if (data < node.data) {
+          node.left = new Node(data);
+        }
+      };
+    }
+  }
+}
