00ced5393a5ad85c238f5b591c88283bcbecc2ce
Kenny Ngo <kenny.ngo.design@gmail.com>
2020-11-23 20:48:07 -0800

    finishing up queue

--- queue.js
+++ queue.js
@@ -1,7 +1,6 @@
 // stack is FILO, queue is FIFO
+collection = [];
 function Queue() {
-  collection = [];
-
   this.print = function () {
     console.log(collection);
   };
@@ -9,4 +8,29 @@ function Queue() {
   this.enqueue = function (element) {
     collection.push(element);
   };
+
+  this.dequeue = function () {
+    return collection.shift();
+  };
+
+  this.front = function () {
+    return collection[0];
+  };
+
+  this.size = function () {
+    return collection.length;
+  };
+
+  this.isEmpty = function () {
+    return collection.length === 0;
+  };
 }
+
+var q = new Queue();
+q.enqueue("a");
+q.enqueue("b");
+q.enqueue("c");
+q.print();
+q.dequeue();
+console.log(q.front());
+q.print();
