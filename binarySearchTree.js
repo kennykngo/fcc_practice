82657e512f9aa63da2db80d0f62d234a1098b11c
Kenny Ngo <kenny.ngo.design@gmail.com>
2020-11-24 15:44:22 -0800

    finished BST

--- binarySearchTree.js
+++ binarySearchTree.js
@@ -92,6 +92,62 @@ class BST {
         current = current.right;
       }
     }
+    // returns false since the value that you're looking for is not in the tree
     return false;
   }
+
+  remove(data) {
+    const removeNode = function (node, data) {
+      // checking if tree is empty
+      if (node == null) {
+        return null;
+      }
+      // if we found the node with the data
+      if (data == node.data) {
+        // node has no children
+        if (node.left == null && node.right == null) {
+          return null;
+        }
+        // node has no left child
+        if (node.left == null) {
+          return node.right;
+        }
+        // node has no right child
+        if (node.right == null) {
+          return node.left;
+        }
+        // node has two children
+        var tempNode = node.right;
+        while (tempNode.left !== null) {
+          tempNode = tempNode.left;
+        }
+        node.data = tempNode.data;
+        node.right = removeNode(node.right, tempNode.data);
+        return node;
+      } else if (data < node.data) {
+        node.left = removeNode(node.left, data);
+        return node;
+      } else {
+        node.right = removeNode(node.right, data);
+        return node;
+      }
+    };
+    this.root = removeNode(this.root, data);
+  }
 }
+
+const bst = new BST();
+
+bst.add(4);
+bst.add(2);
+bst.add(6);
+bst.add(1);
+bst.add(3);
+bst.add(5);
+bst.add(7);
+bst.remove(4);
+console.log(bst.findMin());
+console.log(bst.findMax());
+bst.remove(7);
+console.log(bst.findMax());
+console.log(bst.isPresent(4));
