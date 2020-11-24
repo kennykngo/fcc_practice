9397423fa97babb34d6974e435fbb3bab0c19156
Kenny Ngo <kenny.ngo.design@gmail.com>
2020-11-24 15:16:45 -0800

    finished add() of BST

--- binarySearchTree.js
+++ binarySearchTree.js
@@ -1,3 +1,8 @@
+/* 
+BST has 2 branches for every node
+- Ordered. Each subtree is less than or greater than parent node
+- Uses operation of binary search: time complexity is equal O(log N)    
+*/
 class Node {
   constructor(data, left = null, right = null) {
     this.data = data;
@@ -13,15 +18,41 @@ class BST {
 
   add(data) {
     const node = this.root;
+    // if there is nothing in the root node, assign the new value to the parent
     if (node === null) {
       this.root = new Node(data);
       return;
     } else {
       const searchTree = function (node) {
         if (data < node.data) {
-          node.left = new Node(data);
+          if (node.left === null) {
+            node.left = new Node(data);
+            return;
+          } else if (node.left !== null) {
+            return searchTree(node.left);
+          }
+        } else if (data > node.data) {
+          if (node.right === null) {
+            node.right = new Node(data);
+            return;
+          } else if (node.right !== null) {
+            return searchTree(node.right);
+          }
+        } else {
+          // instances in which the value is equal
+          return null;
         }
       };
+      // initially return the search tree
+      return searchTree(node);
     }
+  }
+
+  findMin() {
+    let current = this.root;
+    while (current.left !== null) {
+      current = current.left;
+    }
+    return current.data;
   }
 }
